64b3d1b42524462b791cef51aa86991202dcddee
manami-project <13179375+manami-project@users.noreply.github.com>
2022-11-03 23:09:59 +0100

    KDoc optimization

--- src/main/kotlin/io/github/manamiproject/kommand/CommandExecutor.kt
+++ src/main/kotlin/io/github/manamiproject/kommand/CommandExecutor.kt
@@ -20,7 +20,10 @@ public interface CommandExecutor {
      * Execute a cli command.
      * @since 1.0.0
      * @param command A [List] containing the command as first element followed by the arguments and their values.
-     * **Example:** `listOf("echo", "-n", "hello world")
+     * **Example:**
+     * ```
+     * listOf("echo", "-n", "hello world")
+     * ```
      * @return Returns the console output of the command as [String]
      * @throws UnsupportedOperatingSystemException If the operating system on which the code is executed is neither Linux nor MacOs.
      * @throws IncorrectPasswordAttemptException If you want to execute a command using `sudo`, but the provided password is incorrect.
